2962fe7cc8b7b35f730d558cc0d5a5b9e993b99c
artsasse <artsasse@gmail.com>
2021-01-24 20:07:48 -0300

    +testing arm rotation

--- threejs/anim_1.js
+++ threejs/anim_1.js
@@ -4,14 +4,12 @@ Object.assign( WaveAnimation.prototype, {
 
     init: function() {
         let upperArmTween = new TWEEN.Tween( {theta:0} )
-            .to( {theta:Math.PI }, 500)
+            .to( {theta:Math.PI/2 }, 500)
             .onUpdate(function(){
                 // This is an example of rotation of the right_upper_arm 
                 // Notice that the transform is M = T * R 
                 let right_upper_arm =  robot.getObjectByName("right_upper_arm");
                 right_upper_arm.matrix.makeRotationZ(this._object.theta).premultiply( new THREE.Matrix4().makeTranslation(2.6, 0, 0 ) );
-
-
 
                 // Updating final world matrix (with parent transforms) - mandatory
                 right_upper_arm.updateMatrixWorld(true);
@@ -20,9 +18,21 @@ Object.assign( WaveAnimation.prototype, {
                 renderer.render(scene, camera);    
             })
         // Here you may include animations for other parts 
-            
-    
         
+        let lowerArmTween = new TWEEN.Tween( {theta:0} )
+            .to( {theta: Math.PI/2 }, 500)
+            .onUpdate(function(){
+                let right_lower_arm =  robot.getObjectByName("right_upper_arm").getObjectByName("lower_arm");
+                right_lower_arm.matrix.makeRotationZ(this._object.theta).premultiply( new THREE.Matrix4().makeTranslation(right_lower_arm.position.x, 0, 0 ) );
+
+                // Updating final world matrix (with parent transforms) - mandatory
+                right_lower_arm.updateMatrixWorld(true);
+                // Updating screen
+                stats.update();
+                renderer.render(scene, camera);    
+            })
+
+        upperArmTween.chain(lowerArmTween);
         //  upperArmTween.chain( ... ); this allows other related Tween animations occur at the same time
         upperArmTween.start();       
     },
