d709ea19cd3a246c2cab9e72996796651dc6bc47
artsasse <artsasse@gmail.com>
2021-01-27 21:50:00 -0300

    +rotate function

--- threejs/anim_1.js
+++ threejs/anim_1.js
@@ -1,5 +1,30 @@
 function WaveAnimation() {}
 
+function rotateAroundPivot(pivot_x, pivot_y, angle, element){
+    ele_mat = element.matrix;
+    ele_mat.makeRotationZ(angle);
+
+    // Como o método makeRotationZ reseta a matriz local do objeto, temos que restaurar
+    // as coordenadas antigas dele com uma translacao.
+    old_x = element.position.x;
+    old_y = element.position.y;
+    old_z = element.position.z;
+    old_position_mat = new THREE.Matrix4().makeTranslation(old_x, old_y, old_z);
+    ele_mat.premultiply(old_position_mat);
+
+    // Criando translacao, colocando o centro de rotacao na origem. 
+    // O centro de rotacao é p = [pivot_x, pivot_y]
+    translate_mat = new THREE.Matrix4().makeTranslation(-pivot_x, -pivot_y, 0);
+
+    // Pos multiplicacao: M = R * T
+    ele_mat.multiply(translate_mat);
+
+    // Desfazendo translacao inicial. Aplicando Pre multiplicacao: M = T^-1 * M. Ou seja: M = T^-1 * R * T
+    // Tivemos que atualizar o three.js para utilizar o método invert()
+    translate_mat.invert();
+    ele_mat.premultiply(translate_mat);
+}
+
 Object.assign( WaveAnimation.prototype, {
 
     init: function() {
@@ -7,28 +32,8 @@ Object.assign( WaveAnimation.prototype, {
             .to( {theta:Math.PI/2, shoulder:2.1 }, 500)
             .onUpdate(function(){
                 let right_upper_arm = robot.getObjectByName("right_upper_arm");
-                arm_mat = right_upper_arm.matrix;
 
-                //centro da rotacao
-                let p1 = -0.5;
-                let p2 = 1.8;
-
-                // Como o método makeRotationZ reseta a matriz local do objeto, temos que restaurar
-                // as coordenadas antigas dele com uma translacao.
-                arm_mat.makeRotationZ(this._object.theta);
-                old_position_mat = new THREE.Matrix4().makeTranslation(right_upper_arm.position.x, right_upper_arm.position.y, right_upper_arm.position.z);
-                arm_mat.premultiply(old_position_mat);
-
-                // Criando translacao, colocando o centro de rot na origem. O centro de rotacao é p = [p1, p2]
-                translate_mat = new THREE.Matrix4().makeTranslation(-p1,-p2, 0);
-
-                // Pos multiplicacao: M = R * T
-                arm_mat.multiply(translate_mat);
-
-                // Desfazendo translacao inicial. Aplicando Pre multiplicacao: M = T^-1 * M. Ou seja: M = T^-1 * R * T
-                // Tivemos que atualizar o three.js para utilizar o método invert()
-                translate_mat.invert();
-                arm_mat.premultiply(translate_mat);
+                rotateAroundPivot(-0.5, 1.8, this._object.theta, right_upper_arm);
 
                 // Segundo os docs do three.js é importante setar para false para evitar problemas
                 // quando mexemos diretamente com as matrizes dos objetos.
